903d17e41a7d533479fb1929d5f8cbf87a367ed6
rahul <rahul@DESKTOP-OSI7PV8>
2023-04-05 09:49:18 +0530

    First Spring github Project

---
+++ springdemo/src/main/java/com/springdemo/App.java
@@ -0,0 +1,20 @@
+package com.springdemo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	 ApplicationContext context = new ClassPathXmlApplicationContext("com/springdemo/config.xml");
+         Student student1= (Student) context.getBean("student1");
+    	 System.out.println( "Hello World!" );
+    	 System.out.println(student1);
+    }
+}
